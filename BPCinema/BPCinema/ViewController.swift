c214e60bf57169e1070576f92c4a99b701c62ddc
Annitas <39757257+Annitas@users.noreply.github.com>
2024-03-18 16:12:44 +0600

    Initial project

---
+++ BPCinema/BPCinema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BPCinema
+//
+//  Created by Anita Stashevskaya on 18.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

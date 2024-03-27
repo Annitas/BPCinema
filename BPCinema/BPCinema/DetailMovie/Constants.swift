f36437f1e8e3fdb9f72636a6e66c0357b39ef667
Annitas <39757257+Annitas@users.noreply.github.com>
2024-03-27 12:13:43 +0600

    Movie UI components into separate file

---
+++ BPCinema/BPCinema/DetailMovie/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  BPCinema
+//
+//  Created by Anita Stashevskaya on 26.03.2024.
+//
+
+import Foundation
---
+++ BPCinema/BPCinema/UIComponents/CustomButton.swift
@@ -0,0 +1,20 @@
+//
+//  CustomButton.swift
+//  BPCinema
+//
+//  Created by Anita Stashevskaya on 27.03.2024.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

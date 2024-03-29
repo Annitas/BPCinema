06bb7ccba8093a5589e79835f489079319e7a718
Annitas <39757257+Annitas@users.noreply.github.com>
2024-03-29 12:02:59 +0600

    Add Get Favourites method to API Service

--- BPCinema/BPCinema/APIService.swift
+++ BPCinema/BPCinema/APIService.swift
@@ -9,7 +9,9 @@ import Foundation
 
 final class APIService {
     static let shared = APIService()
+    
     // MARK: List of movies
+    
     func getListOfMoview() async -> PopularMovieResponseEntity {
         let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=fae05adc59b94dcb33377a38bfd09528&page=1")!
         let (data, _) = try! await URLSession.shared.data(from: url)
@@ -17,6 +19,7 @@ final class APIService {
     }
     
     // MARK: Detail Movies
+    
     func getDetailMovie(withID id: String) async -> DetailMovieEntity {
         let url = URL(string: "https://api.themoviedb.org/3/movie/\(id)?api_key=fae05adc59b94dcb33377a38bfd09528")!
         let (data, _) = try! await URLSession.shared.data(from: url)
@@ -24,6 +27,8 @@ final class APIService {
         jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
         return try! jsonDecoder.decode(DetailMovieEntity.self, from: data)
     }
+    
+    // MARK: Favourites
     
     func addToFavorites(movieId: String, accountId: String) {
         let urlString = "https://api.themoviedb.org/3/account/\(accountId)/favorite?api_key=fae05adc59b94dcb33377a38bfd09528"
@@ -59,6 +64,53 @@ final class APIService {
               print(httpResponse ?? "")
           }
         })
+        dataTask.resume()
+    }
+    
+    // call
+//    getFavouriteMovies { result in
+//        switch result {
+//        case .success(let popularMovieResponse):
+//            // Обработать успешный результат
+//            print(popularMovieResponse)
+//        case .failure(let error):
+//            // Обработать ошибку
+//            print("Ошибка получения избранных фильмов: \(error)")
+//        }
+//    }
+    
+    func getFavouriteMovies(completion: @escaping (Result<PopularMovieResponseEntity, Error>) -> Void) {
+        let headers = [
+            "accept": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYWUwNWFkYzU5Yjk0ZGNiMzMzNzdhMzhiZmQwOTUyOCIsInN1YiI6IjY1ZjE0NzIyMmZkZWM2MDE4OTIxMzFmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NqkryGcwJ1BvUn_id9-DGJgpL_wm2stm4FTC4p5cEVQ"
+        ]
+
+        let request = NSMutableURLRequest(url: NSURL(string: "https://api.themoviedb.org/3/account/21098921/favorite/movies?language=en-US&page=1&sort_by=created_at.asc")! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) {
+                do {
+                    let decoder = JSONDecoder()
+                    let popularMovieResponse = try decoder.decode(PopularMovieResponseEntity.self, from: data!)
+                    completion(.success(popularMovieResponse))
+                } catch {
+                    completion(.failure(error))
+                }
+            } else {
+                completion(.failure(NSError(domain: "HTTPError", code: (response as? HTTPURLResponse)?.statusCode ?? 500, userInfo: nil)))
+            }
+        }
+        
         dataTask.resume()
     }
 }

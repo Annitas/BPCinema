91081291172c2253b8c424adf090985b024abfb9
Annitas <39757257+Annitas@users.noreply.github.com>
2024-04-08 14:38:24 +0600

    Add login factory

--- BPCinema/BPCinema/Login/LoginFactory.swift
+++ BPCinema/BPCinema/Login/LoginFactory.swift
@@ -5,14 +5,15 @@
 //  Created by Anita Stashevskaya on 08.04.2024.
 //
 
-import Foundation
+import UIKit
 
 final class LoginFactory {
-    static func assembledScreen(withRouter router: LoginRouter) { // server, router, -> loginVC
-//        let interactor = LoginInteractor()
-//        let presenter = LoginPresenter()
-//        let viewController = LoginViewController()
-//        router.viewController = viewController
-//        return viewController
+    static func assembledScreen(withRouter router: LoginRouter) {
+        let presenter = LoginPresenter(router: router)
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let window = windowScene.windows.first else {
+            return
+        }
+        router.showLoginView(window: window)
     }
 }
--- BPCinema/BPCinema/SceneDelegate.swift
+++ BPCinema/BPCinema/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        loginRouter.showLoginView(window: window)
+        LoginFactory.assembledScreen(withRouter: loginRouter)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
